459173cf2270e1723ee377205de278c7a52f7ec2
David Moore <hey@iamdavidmoore.com>
2018-04-27 16:36:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,10 +103,11 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, seasons_hash|
     seasons_hash.each do |holiday, supply_array|
+      bbq = []
       if supply_array.include?("BBQ")
-        holiday
+        bbq << holiday.key(supply_array)
       end
     end
   end
-  holiday
+  bbq
 end
